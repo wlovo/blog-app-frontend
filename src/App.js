5aab031845ae76a3a023866950db3dbc9f6814a1
wlovo <will9637@gmail.com>
2019-09-29 11:48:11 -0400

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import AppWrapper from './components/AppWrapper';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <AppWrapper />
+      <BrowserRouter>
+        <AppWrapper />
+      </BrowserRouter>
     </div>
   );
 }
