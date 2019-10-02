6dc64c5a0f74cf4b5e7f001802d75ca160a042f1
wlovo <will9637@gmail.com>
2019-10-02 00:21:06 -0400

    Change to functional component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import { Route } from 'react-router-dom';
 import PageHome from './Page.Home';
 import PagePost from '../containers/Page.Post';
 
-class MainContent extends Component {
-  render() {
-    return (
-      <Container>
-        <Route exact path="/comments">
-          <h1>Comments</h1>
-        </Route>
-        <Route exact path="/posts">
-          <h1>Posts</h1>
-          <PagePost />
-        </Route>
-        <Route exact path="/">
-          <PageHome />
-        </Route>
-      </Container>
-    );
-  }
-}
+const MainContent = () => (
+  <Container>
+    <Route exact path="/comments">
+      <h1>Comments</h1>
+    </Route>
+    <Route exact path="/posts">
+      <h1>Posts</h1>
+      <PagePost />
+    </Route>
+    <Route exact path="/">
+      <PageHome />
+    </Route>
+  </Container>
+);
 
 export default MainContent;
