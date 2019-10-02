b9f5c71d3764d20205b8582fcf7f208ca9f8edea
wlovo <will9637@gmail.com>
2019-10-01 23:54:06 -0400

    Change to functional component

--- src/components/AppWrapper.js
+++ src/components/AppWrapper.js
@@ -1,25 +1,21 @@
-import React, { Suspense, Component } from 'react';
+import React, { Suspense } from 'react';
 import { Switch } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Header from './Header';
-// import MainContent from './MainContent'
 import Footer from './Footer';
+
 const MainContent = React.lazy(() => import('./MainContent'));
 
-class AppWrapper extends Component {
-  render() {
-    return (
-      <Container>
-        <Header />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <MainContent />
-          </Switch>
-        </Suspense>
-        <Footer />
-      </Container>
-    );
-  }
-}
+const AppWrapper = () => (
+  <Container>
+    <Header />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <MainContent />
+      </Switch>
+    </Suspense>
+    <Footer />
+  </Container>
+);
 
 export default AppWrapper;
